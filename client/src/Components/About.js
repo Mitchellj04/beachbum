323821c488fe5f783659c6b2529ebab7bd9e734f
JustinMitchell <mitchelljm04@gmail.com>
2023-05-22 14:49:40 -0400

    About updated

--- client/src/Components/About.js
+++ client/src/Components/About.js
@@ -2,23 +2,32 @@ import { Box, Grid, Typography } from '@mui/material'
 import React from 'react'
 import beach from '../Images/8mile-beach.JPG'
 import me from '../Images/koz-top.JPG'
+import lighthouse from '../Images/lighthouse.JPG'
 
 const About = () => {
   return (
     <div style={{marginTop: '10vh'}}>
 
-        <img src={beach} style={{width: '100vw', height: '100vh'}}></img>        
+        <img src={beach} style={{width: '100vw', height: '100vh'}}></img>    
+        <div style={{justifyContent: 'center', alignItems: 'center'}}>   
         <Typography>Are you a laid back guy or gal?</Typography>
         <Typography>Is all you want to do is relax on the beach? </Typography>
+        </div> 
         <Box style={{maringTop: '20vh'}}>
             <Grid container style={{justifyContent: 'center', alignItems: 'center'}}>
                 <Grid item xs={5}>
                     <img src={me} style={{width: '100%', height: '100%'}}></img>
                 </Grid>
                 <Grid item xs={5}>
-                    Do you just wish to just sit on the beach every day. Are you constantly chasing those amazing sunsets? 
+                    Hello I am Justin Mithell founder of Beach Bum. I am an outdoor enthuesist always seeking that next adventure. I have had my fair share of sunsets, sunrises and just overall good times with good people.  
                 </Grid>
-                <Grid
+                <Grid item xs={5}>
+                    I was stuck with a problem trying to find nice cloths that encapsulated my adventurous and relaxed personality. Either the cloths were just too cheap and are not breathable or they were extermely expensive for what you are getting. 
+                    
+                </Grid>
+                <Grid item xs={5}>
+                    <img src={lighthouse} style={{width: '100%', height: '100%'}}></img>
+                </Grid>
             </Grid>
         </Box>
     </div>
--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -2,13 +2,16 @@ import React from 'react'
 import '../Home.css'
 import beach from '../Images/beach-background.jpeg'
 import surf from '../Images/pxfuel.jpg'
+import { Typography } from '@mui/material'
 
 const Home = () => {
   return (
     <div className='home'>
         <img src={surf} className='main-image' />
 
-        <div></div>
+        <div>
+            <Typography></Typography>
+        </div>
     </div>
   )
 }
