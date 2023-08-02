5d021e12ca244b9edbee1e25cc547969a2d1c953
JustinMitchell <mitchelljm04@gmail.com>
2023-08-02 14:32:23 -0400

    Checkout Removed

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,20 +1,20 @@
 class CheckoutController < ApplicationController
 
-    def create
-        product = Product.find_by(id: params[:product_id])
-        session = Stripe::Checkout::Session.create({
-            payment_method_types: ['card'],
-            line_items: [{
-                name: product.name
-                amount: product.price,
-                quantity: 1,
-                currency: 'usd',
-                }],
-            mode: 'payment',
-            success_url: root_url,
-            cancel_url: root_url,
-            })
-    end
+    # def create
+    #     product = Product.find_by(id: params[:product_id])
+    #     session = Stripe::Checkout::Session.create({
+    #         payment_method_types: ['card'],
+    #         line_items: [{
+    #             name: product.name
+    #             amount: product.price,
+    #             quantity: 1,
+    #             currency: 'usd',
+    #             }],
+    #         mode: 'payment',
+    #         success_url: root_url,
+    #         cancel_url: root_url,
+    #         })
+    # end
 
 
 
