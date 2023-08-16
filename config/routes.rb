98f1f16f55fa05e5843d65338a00d36d43f41408
JustinMitchell <mitchelljm04@gmail.com>
2023-08-16 16:04:12 -0400

    routes changed

--- config/routes.rb
+++ config/routes.rb
@@ -18,4 +18,5 @@ Rails.application.routes.draw do
 
   get "*path", to: "application#index", constraints: ->(req) { !req.xhr? && req.format.html? }
   get '/admin', to: "application#admin", constraints: ->(req) { !req.xhr? && req.format.html? }
+  get '/admin', to: "application#admin"
 end
